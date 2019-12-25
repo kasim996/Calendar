e30a6d70f6ca11d844891517016ce06db22eb009
pylmcy150 <pylmcy150@sina.cn>
2019-12-26 01:13:24 +0800

    完成Note基本功能

--- calendar/src/components/StickyNotes.vue
+++ calendar/src/components/StickyNotes.vue
@@ -4,19 +4,96 @@
       <img :src="xUrl" />
     </div>
     <ul class="note-view">
-      <li>
-          <ul class="note-ul">
-              <li class="note-li-num"></li>
-              <li class="note-li-content"><textarea >这是一个测试活动活动活动活动活动活动活动活动活动活动活动活动</textarea></li>
-              <li class="note-li-level">123</li>
-          </ul>
+      <li v-for="(item,i) in taskDatas" :key="i">
+        <ul class="note-ul">
+          <li class="note-li-num">{{item.taskNum}}</li>
+          <li class="note-li-content">
+            <textarea
+              v-model.trim="item.taskValue"
+              :class="item.taskStatus?'note-li-content-ok':''"
+            ></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
       </li>
-      <li>123123</li>
-      <li>123123</li>
-      <li>123123</li>
-      <li>123123</li>
-      <li>123123</li>
-      <li>123123</li>
+      <!-- <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">一、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>
+      <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">二、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>
+      <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">三、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>
+      <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">四、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>
+      <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">五、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>
+      <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">六、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>
+      <li>
+        <ul class="note-ul clear">
+          <li class="note-li-num clear">七、</li>
+          <li class="note-li-content">
+            <textarea></textarea>
+          </li>
+          <li class="note-li-level"></li>
+        </ul>
+      </li>-->
+    </ul>
+    <ul class="note-task">
+      <!-- <li class="task-high-urgent"></li>
+      <li class="task-high-normal"></li>
+      <li class="task-high-normal"></li>
+      <li class="task-high-normal"></li>
+      <li class="task-low-urgent"></li>
+      <li class="task-low-urgent"></li>
+      <li class="task-low-normal"></li>-->
+      <li
+        v-for="(item,index) in taskStatusList"
+        :class="item.taskLvl"
+        :key="index"
+        @click="finishTask(item)"
+      ></li>
     </ul>
   </div>
 </template>
@@ -25,8 +102,73 @@ import x_icon from "../assets/icon/x-icon.png";
 export default {
   data() {
     return {
-      xUrl: x_icon
+      xUrl: x_icon,
+      taskDatas: []
     };
+  },
+  watch: {
+    taskDatas: {
+      handler: function(newValue) {
+        newValue.forEach(function(v) {
+          v.taskStatus = v.taskValue == "" ? null : v.taskStatus ? true : false;
+        });
+        return newValue;
+      },
+      deep: true
+    }
+  },
+  computed: {
+    taskStatusList: function() {
+      let arr = [];
+      let taskLvls = [
+        "task-high-urgent",
+        "task-high-normal",
+        "task-high-normal",
+        "task-high-normal",
+        "task-low-urgent",
+        "task-low-urgent",
+        "task-low-normal"
+      ];
+      this.taskDatas.forEach(function(v, i) {
+        if (v.taskStatus === true) {
+          arr.push({
+            taskLvl: `${taskLvls[i]} task-finish`,
+            taskIndex: i
+          });
+        }
+        if (v.taskStatus === false) {
+          arr.unshift({
+            taskLvl: `${taskLvls[i]} task-start`,
+            taskIndex: i
+          });
+        }
+      });
+      return arr;
+    }
+  },
+  methods: {
+    initTasks: function() {
+      let taskDatas = [];
+      let nums = ["一、", "二、", "三、", "四、", "五、", "六、", "七、"];
+      nums.forEach(function(v, i) {
+        taskDatas.push({
+          taskIndex: i,
+          taskNum: v,
+          taskValue: "",
+          taskStatus: null
+        });
+      });
+      return taskDatas;
+    },
+    finishTask(task) {
+      this.taskDatas[task.taskIndex].taskStatus = !this.taskDatas[
+        task.taskIndex
+      ].taskStatus;
+      this.$set(this.taskDatas, task.taskIndex, this.taskDatas[task.taskIndex]);
+    }
+  },
+  created: function() {
+    this.taskDatas = this.initTasks();
   }
 };
 </script>
@@ -48,38 +190,115 @@ export default {
 }
 .note-view {
   padding: 0;
-  margin-top: 70px;
+  margin: 70px 0px 0px 0px;
   list-style: none;
 }
-.note-view > li{
-    height: 70px;
-    line-height: 70px;
-    border: 1px solid red;
+.note-view > li {
+  height: 70px;
+  line-height: 70px;
+  // border: 1px solid red;
 }
 .note-ul {
   padding: 0;
-  margin:0 auto;
+  margin: 0 auto;
   list-style: none;
   width: 900px;
   height: 100%;
 }
-.note-ul > li{
-    float: left;
+.note-ul > li {
+  height: 100%;
+  font-size: 25px;
+  float: left;
+  padding: 0px;
+  margin: 0px;
+  line-height: 40px;
+  font-family: "Microsoft YaHei";
+  color: #2b2b2b;
 }
-.note-li-num{
-    width: 50px;
+.note-li-num {
+  width: 50px;
+  text-align: right;
 }
-.note-li-content{
-    width: 800px;
+.note-li-content {
+  width: 800px;
 }
-.note-li-content > textarea{
-    width: 800px;
-    height: 65px;
-    border: none;
-    background-color: #fdf5e6;
-    margin-top:1px;
+.note-li-content > textarea {
+  width: 100%;
+  height: 64px;
+  // border: 0;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #ededed;
+  background-color: #fdf5e6;
+  margin-top: 1px;
+  resize: none;
+  outline: none;
+  font-size: 25px;
+  font-family: "Microsoft YaHei";
+  color: #2b2b2b;
 }
-.note-li-level{
-    width: 50px;
+.note-li-content-ok{
+  border-bottom: 1px solid #CDCDB4 !important;
+}
+textarea::-webkit-scrollbar {
+  width: 5px;
+}
+textarea::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius: 10px;
+  box-shadow: inset 0 0 5px transparent;
+  background: transparent;
+}
+textarea::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow: inset 0 0 5px transparent;
+  border-radius: 10px;
+  background: transparent;
+}
+.note-li-level {
+  width: 40px;
+  height: 80%;
+}
+.note-task {
+  margin-top: 45px;
+  margin-bottom: 0px;
+  padding: 0;
+  // float: right;
+  list-style: none;
+  width: 100%;
+  height: 5px;
+}
+.note-task > li {
+  // float: right;
+  width: 40px;
+  border: none;
+  height: 100%;
+  cursor: pointer;
+}
+.task-high-urgent {
+  // border-left: 10px solid #ee3b3b;
+  // float: left;
+  background-color: #ee3b3b;
+}
+.task-high-normal {
+  // float: right;
+  // border-top: 15px solid #ffb90f;
+  background-color: #ffb90f;
+}
+.task-low-urgent {
+  // float: right;
+  // border-top: 15px solid #4682b4;
+  background-color: #4682b4;
+}
+.task-low-normal {
+  // border-top: 15px solid #828282;
+  background-color: #828282;
+}
+.task-finish {
+  float: left;
+}
+.task-start {
+  float: right;
 }
 </style>
